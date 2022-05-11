9aca78453edb85fb01836603c959343b29cf792a
Cameron <sicklecj@gmail.com>
2022-05-11 09:43:33 -0500

    set up routes

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const router = require('express').Router();
+
+const notes = require('../../db/db.json')
+
+router.get('/notes', (req, res) => {
+
+    res.json(notes);
+
+})
+
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const app = express();
 
 const PORT = process.env.PORT || 3001;
 
+const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
 app.use(express.static('public'));
 
+app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {

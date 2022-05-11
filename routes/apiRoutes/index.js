2264f61b2c5b0c1ede4f9350bc043d231dce6807
Cameron <sicklecj@gmail.com>
2022-05-11 09:56:50 -0500

    psuedo code for planning remainder of router.get

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,14 +1,26 @@
 const fs = require('fs');
+const path = require('path');
 const router = require('express').Router();
 
-const notes = require('../../db/db.json')
 
 router.get('/notes', (req, res) => {
 
-    res.json(notes);
 
-})
+    fs.readFile(path.join(__dirname, '../../db/db.json'), 'UTF-8', (err, data) => {
+        if (err) throw err
+        console.log(data);
+    })
+
+    // res.json(notes);
+
+});
+
+
 
 
 
 module.exports = router;
+
+//fs readfile does a string. Will need to covert to usable data type
+
+// with the router.get, router.post needs to use fs.writeFile to push content to db.json

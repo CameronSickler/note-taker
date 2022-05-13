b3aabe300d2d8345bbb31fb4f1d8dc83afd44ce9
Cameron <sicklecj@gmail.com>
2022-05-13 16:13:06 -0500

    add fs write file to api router

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -2,23 +2,27 @@ const fs = require('fs');
 const path = require('path');
 const router = require('express').Router();
 
-
 router.get('/notes', (req, res) => {
 
 
     fs.readFile(path.join(__dirname, '../../db/db.json'), 'UTF-8', (err, data) => {
         if (err) throw err
-        console.log(data);
+        // console.log(data);
+        res.json(data);
     });
 
-    // res.json(data);
+
+});
 
 
+router.post('/notes', (req, res) => {
 
-    // fs.writeFile('./notes', data, function (err) {
-    //     if (err) throw err;
-    //     console.log('Saved!');
-    // });
+    fs.writeFile(path.join(__dirname, '../../db/db.json'), 'UTF-8', (err, data) => {
+        if (err) throw err
+        // console.log(data);
+        res.json(req.body);
+    });
+
 
 });
 

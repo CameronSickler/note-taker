12e71460a5670cd12634a54e9112084c9a29fc80
Cameron <sicklecj@gmail.com>
2022-05-13 18:01:53 -0500

    discover that res sendfile gets note to display on browser

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -2,32 +2,27 @@ const fs = require('fs');
 const path = require('path');
 const router = require('express').Router();
 
+const dbFilePath = path.join(__dirname, '../../db/db.json');
+
 router.get('/notes', (req, res) => {
 
-
-    fs.readFile(path.join(__dirname, '../../db/db.json'), 'UTF-8', (err, data) => {
-        if (err) throw err
-        // console.log(data);
-        res.json(data);
-    });
-
-
-});
-
-
-router.post('/notes', (req, res) => {
-
-    fs.writeFile(path.join(__dirname, '../../db/db.json'), 'UTF-8', (err, data) => {
-        if (err) throw err
-        // console.log(data);
-        res.json(req.body);
-    });
+    res.sendFile(dbFilePath);
+    // fs.readFile(dbFilePath, 'UTF-8', (err, data) => {
+    //     if (err) throw err
+    //     console.log(data);
+    //     res.json(data);
+    // });
 
 
 });
 
 
+// router.post('/notes', (req, res) => {
 
+//     console.log(req.body);
+//     res.json(req.body);
+
+// });
 
 
 module.exports = router;
